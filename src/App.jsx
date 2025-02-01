e78d076cdfd3db8b9cac880e5771216594c45c52
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-01 19:50:55 +0530

    vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="mx-auto flex bg-black">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline text-red-500">
+          Hello, Tailwind CSS!
+        </h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
